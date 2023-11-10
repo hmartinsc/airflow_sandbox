8eb7f1dd2a41dcfac5131a5343a2acd77465e9ad
hmartinc <hmartins19@gmail.com>
2023-11-10 02:29:58 -0300

    entire forex pipeline

--- forex-pipeline/mnt/airflow/dags/forex_data_pipeline.py
+++ forex-pipeline/mnt/airflow/dags/forex_data_pipeline.py
@@ -1,3 +1,8 @@
+from datetime import datetime, timedelta
+import csv
+import requests
+import json
+
 from airflow import DAG
 from airflow.providers.http.sensors.http import HttpSensor
 from airflow.sensors.filesystem import FileSensor
@@ -6,22 +11,32 @@ from airflow.operators.bash import BashOperator
 from airflow.providers.apache.hive.operators.hive import HiveOperator
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from airflow.operators.email import EmailOperator
-from airflow.providers.slack.operators.slack_webhook import SlackWebhookOperator
 
-from datetime import datetime, timedelta
-import csv
-import requests
-import json
+DAG_ID = "forex_data_pipeline"
 
+"""
+default_args are the default arguments applied to all tasks in the DAG
+
+owner:              is the owner of the DAG
+email_on_failure:   is set to True to receive email alerts on failure
+email_on_retry:     is set to False to not receive email alerts on retry
+email:              is the email to receive alerts
+retries:            is the number of retries that should be performed before failing the task
+retry_delay:        is the time to wait between retries
+"""
 default_args = {
     "owner": "airflow",
     "email_on_failure": False,
     "email_on_retry": False,
-    "email": "admin@localhost.com",
+    "email": "example@axample.com",
     "retries": 1,
     "retry_delay": timedelta(minutes=5)
 }
 
+"""
+Download forex rates according to the currencies we want to watch
+described in the file forex_currencies.csv
+"""
 def download_rates():
     BASE_URL = "https://gist.githubusercontent.com/marclamberti/f45f872dea4dfd3eaa015a4a1af4b39b/raw/"
     ENDPOINTS = {
@@ -41,12 +56,39 @@ def download_rates():
                 json.dump(outdata, outfile)
                 outfile.write('\n')
 
-def _get_message() -> str:
-    return "Hi from forex_data_pipeline"
+"""
+DAG is the core concept of Airflow. It is a collection of tasks that you want to run,
+organized in a way that reflects their relationships and dependencies.
 
-with DAG("forex_data_pipeline", start_date=datetime(2021, 1 ,1), 
-    schedule_interval="@daily", default_args=default_args, catchup=False) as dag:
+Arguments:
 
+dag_id:             must be unique across all DAGs
+default_args:       are the default arguments applied to all tasks in the DAG
+tags:               are used to categorize DAGs
+start_date:         is the date when the DAG will start running
+schedule_interval:  is the frequency with which the DAG will be run
+catchup:            is set to False to prevent backfilling of the DAG
+"""
+with DAG(
+    dag_id=DAG_ID,
+    default_args=default_args,
+    tags=["forex"],
+    start_date=datetime(2023, 11, 1),
+    schedule_interval="@daily",
+    catchup=False
+) as dag:
+
+    """
+    is_forex_rates_available is a sensor that will wait for the API to be available
+
+    Arguments:
+
+    http_conn_id:   is the connection ID defined in the Airflow UI {conn_id="forex_api", conn_type="http", host="https://gist.github.com"}
+    endpoint:       is the endpoint of the API
+    response_check: is a function that will check the response of the API
+    poke_interval:  is the time between each attempt to poke the API
+    timeout:        is the time to wait before failing the task
+    """
     is_forex_rates_available = HttpSensor(
         task_id="is_forex_rates_available",
         http_conn_id="forex_api",
@@ -56,6 +98,16 @@ with DAG("forex_data_pipeline", start_date=datetime(2021, 1 ,1),
         timeout=20
     )
 
+    """
+    is_forex_currencies_file_available is a sensor that will wait for the file to be available
+
+    Arguments:
+
+    fs_conn_id:     is the connection ID defined in the Airflow UI {conn_id="forex_path", conn_type="file", extra={"path":"/opt/airflow/dags/files"}}
+    filepath:       is the path of the file to check
+    poke_interval:  is the time between each attempt to poke the API
+    timeout:        is the time to wait before failing the task
+    """
     is_forex_currencies_file_available = FileSensor(
         task_id="is_forex_currencies_file_available",
         fs_conn_id="forex_path",
@@ -64,11 +116,25 @@ with DAG("forex_data_pipeline", start_date=datetime(2021, 1 ,1),
         timeout=20
     )
 
+    """
+    download_rates is a PythonOperator that will download the rates from the API
+
+    Arguments:
+
+    python_callable:    is the function to call
+    """
     downloading_rates = PythonOperator(
         task_id="downloading_rates",
         python_callable=download_rates
     )
 
+    """
+    saving_rates is a BashOperator that will save the rates in HDFS
+
+    Arguments:
+
+    bash_command:   is the command to execute
+    """
     saving_rates = BashOperator(
         task_id="saving_rates",
         bash_command="""
@@ -77,11 +143,19 @@ with DAG("forex_data_pipeline", start_date=datetime(2021, 1 ,1),
         """
     )
 
+    """
+    creating_forex_rates_table is a HiveOperator that will create the forex_rates table
+
+    Arguments:
+
+    hive_cli_conn_id:   is the connection ID defined in the Airflow UI {conn_id="hive_conn", conn_type="hive", host="hive", port=10000}
+    hql:    is the Hive query to execute
+    """
     creating_forex_rates_table = HiveOperator(
         task_id="creating_forex_rates_table",
         hive_cli_conn_id="hive_conn",
         hql="""
-            CREATE EXTERNAL TABLE IF NOT EXISTS forex_rates(
+            CREATE EXTERNAL TABLE IF NOT EXISTS forex_rates (
                 base STRING,
                 last_update DATE,
                 eur DOUBLE,
@@ -90,34 +164,48 @@ with DAG("forex_data_pipeline", start_date=datetime(2021, 1 ,1),
                 gbp DOUBLE,
                 jpy DOUBLE,
                 cad DOUBLE
-                )
+            )
             ROW FORMAT DELIMITED
             FIELDS TERMINATED BY ','
             STORED AS TEXTFILE
         """
     )
+    
+    """
+    forex_processing is a SparkSubmitOperator that will process the forex rates
 
+    Arguments:
+
+    application:    is the path to the application to submit
+    conn_id:        is the connection ID defined in the Airflow UI {conn_id="spark_conn", conn_type="spark", host="spark://spark:7077", port=7077}
+    verbose:        is set to False to not display logs
+    """
     forex_processing = SparkSubmitOperator(
         task_id="forex_processing",
         application="/opt/airflow/dags/scripts/forex_processing.py",
         conn_id="spark_conn",
         verbose=False
     )
+    
+    """
+    sending_email_notification is an EmailOperator that will send an email notification
 
-    send_email_notification = EmailOperator(
-        task_id="send_email_notification",
-        to="airflow_course@yopmail.com",
-        subject="forex_data_pipeline",
-        html_content="<h3>forex_data_pipeline</h3>"
-    )
+    Arguments:
 
-    send_slack_notification = SlackWebhookOperator(
-        task_id="send_slack_notification",
-        http_conn_id="slack_conn",
-        message=_get_message(),
-        channel="#monitoring"
+    to:             is the email to send the notification to
+    subject:        is the subject of the email
+    html_content:   is the content of the email
+    """
+    sending_email_notification = EmailOperator(
+        task_id="sending_email_notification",
+        to="hmartins19@gmail.com",	
+        subject="Forex processing completed",
+        html_content="<h3>Forex processing completed</h3>"
     )
     
-    is_forex_rates_available >> is_forex_currencies_file_available >> downloading_rates >> saving_rates 
-    saving_rates >> creating_forex_rates_table >> forex_processing
-    forex_processing >> send_email_notification >> send_slack_notification 
+    is_forex_rates_available >> is_forex_currencies_file_available
+    is_forex_currencies_file_available >> downloading_rates
+    downloading_rates >> saving_rates
+    saving_rates >> creating_forex_rates_table
+    creating_forex_rates_table >> forex_processing
+    forex_processing >> sending_email_notification
